da537eaecf15944c3da8e798ff831b9487aa7409
MUYE CHEN <2837065031@qq.com>
2025-05-07 10:14:33 -0700

    setting page and its functionality aviliable

--- main.py
+++ main.py
@@ -5,19 +5,23 @@ import secrets
 import bcrypt
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project1.db"
 db = SQLAlchemy(app)
+
 
 def generate_salt():
     return secrets.token_hex(16)
+
 
 def hash_password(password: str, salt: str) -> str:
     salted_password = salt + password
     return hex(hash(salted_password))[2:]
 
+
 def verify_password(stored_hash: str, stored_salt: str, input_password: str) -> bool:
     test_hash = hash_password(input_password, stored_salt)
     return secrets.compare_digest(stored_hash, test_hash)
+
 
 class Question(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -29,6 +33,7 @@ class Question(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     answers = db.relationship('Answer', backref='question', lazy=True)
 
+
 class Answer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.Text, nullable=False)
@@ -37,10 +42,13 @@ class Answer(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     question_id = db.Column(db.Integer, db.ForeignKey('question.id'), nullable=False)
 
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(), unique=True, nullable=False)
+    displayed_name = db.Column(db.String(), nullable=True, default='')
     hashed_password = db.Column(db.String(), nullable=False)
+    email = db.Column(db.String(), nullable=True, default='')
     salt = db.Column(db.String(), nullable=False)
     description = db.Column(db.String(), nullable=True)
 
@@ -51,36 +59,49 @@ class User(db.Model):
 with app.app_context():
     db.create_all()
 
+
 @app.route('/')
 def index():
     return render_template("index.html")
+
 
 @app.route('/index')
 def home():
     return render_template("index.html")
 
+
 @app.route('/userhome')
 def userhome():
     return render_template("userhome.html")
+
 
 @app.route('/login')
 def login():
     return render_template("login.html")
 
+
 @app.route('/signup')
 def create_user_html():
     return render_template("signup.html")
+
 
 @app.route('/profile')
 def profile():
     return render_template("profile.html")
 
+
 @app.route('/settings')
 def settings():
     return render_template("settings.html")
 
-@app.route('/profile/<username>')
-def user_profile(username):
+
+@app.route('/profile/me', methods=['POST'])
+def user_profile():
+    if not request.is_json:
+        return jsonify({"error": "Request must be JSON"}), 400
+
+    data = request.get_json()
+    username = data["username"]
     user = User.query.filter_by(username=username).first()
     user_questions = Question.query.filter_by(user_id=user.id).all()
     user_answers = Answer.query.filter_by(user_id=user.id).all()
@@ -91,11 +112,48 @@ def user_profile(username):
         liked += answer.upvotes
     return jsonify({
         'username': user.username,
+        'displayed_name': user.displayed_name,
         'description': user.description,
         'questions': len(user_questions),
         'answers': len(user_answers),
         'liked': liked
-    })
+    }), 200
+
+
+@app.route('/settings/me', methods=['POST', 'PUT'])
+def user_settings():
+    if not request.is_json:
+        return jsonify({"error": "Request must be JSON"}), 400
+
+    if request.method == 'POST':
+        data = request.get_json()
+        username = data["username"]
+        user = User.query.filter_by(username=username).first()
+        user_displayed_name = user.displayed_name
+        user_email = user.email
+        user_description = user.description
+        return jsonify({
+            'displayed_name': user_displayed_name,
+            'email': user_email,
+            'description': user_description,
+        }), 200
+
+    data = request.get_json()
+    username = data["username"]
+    displayed_name = data["displayed_name"]
+    email = data["email"]
+    description = data["description"]
+
+    if not User.query.filter_by(username=username).first():
+        return jsonify({"error": "User does not exist"}), 400
+
+    user = User.query.filter_by(username=username).first()
+    user.displayed_name = displayed_name
+    user.email = email
+    user.description = description
+    db.session.commit()
+    return jsonify({"success": "Successfully changed settings"}), 200
+
 
 @app.route('/user/signin', methods=['POST'])
 def sign_in():
@@ -137,12 +195,14 @@ def create_user():
 
     user = User(
         username=username,
+        displayed_name=username,
         hashed_password=hashed_password,
         salt=salt,
     )
     db.session.add(user)
     db.session.commit()
     return jsonify({"success": "User created successfully"}), 200
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/app.js
+++ static/app.js
@@ -82,12 +82,17 @@ function signUp() {
 document.addEventListener('DOMContentLoaded',
     function openUserProfile() {
     let username = getCookie('username')
-                    console.log(username)
-
-    fetch(`${url}/profile/${username}`)
+    fetch(`${url}/profile/me`, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            "username": username,
+            })
+        })
         .then(response => response.json())
         .then(data => {
-            document.getElementById('username').innerHTML = data.username;
+            document.getElementById('username').innerHTML = ('(username:    ' + data.username + ')');
+            document.getElementById('displayed_name').innerHTML = data.displayed_name;
             document.getElementById('description').innerHTML = data.description;
             document.getElementById('totalQuestions').innerHTML = data.questions;
             document.getElementById('totalAnswers').innerHTML = data.answers;
@@ -95,6 +100,53 @@ document.addEventListener('DOMContentLoaded',
         })
         .catch(err => console.error(err));
 });
+
+document.addEventListener('DOMContentLoaded',
+    function openUserSettings() {
+    let username = getCookie('username');
+
+    fetch(`${url}/settings/me`, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            "username": username,
+            })
+        })
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('displayed_name_in_settings').value = data.displayed_name;
+            document.getElementById('email_in_settings').value = data.email;
+            document.getElementById('description_in_settings').value = data.description;
+        })
+        .catch(err => console.error(err));
+});
+
+function change_user_settings() {
+    const displayed_name = document.getElementById('displayed_name_in_settings').value;
+    const email =document.getElementById('email_in_settings').value;
+    const description =document.getElementById('description_in_settings').value;
+    let username = getCookie('username');
+
+    fetch(`${url}/settings/me`, {
+        method: 'PUT',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            "username": username,
+            "displayed_name": displayed_name,
+            "email": email,
+            "description": description,
+            })
+        })
+        .then(response => {
+            if (response.status != 400) {
+                window.location.href = "profile";
+            } else {
+                window.location.href = "settings";
+            }
+            return response.json()
+        })
+        .catch(err => console.error(err));
+}
 
 function logout() {
     document.cookie = `username=; login=`;
