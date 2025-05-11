8358761d997a6fadcca30a419062294f1e27f1b1
MUYE CHEN <2837065031@qq.com>
2025-05-11 13:51:39 -0700

    minor change

--- static/app.js
+++ static/app.js
@@ -250,7 +250,8 @@ document.addEventListener('DOMContentLoaded',
             <div class="question-actions" id="options">
                 <button class="action-btn">${data["upvotes"]} Approval</button>
                 <button class="action-btn">${data["question_total_answers"]} Answers</button>
-                <button class="action-btn" type="button" id="like_button" onclick="upvote_question()">❤ Like</button>
+                <button type="button" id="like_button" class="action-btn" onclick="
+                    document.cookie = 'question_title=${question_title}';upvote_question()">❤ Like</button>
             </div>`;
                 question_container.innerHTML += item;
 
